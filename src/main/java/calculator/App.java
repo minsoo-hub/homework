bb93168d56d5dd618cf6031cf4e12c4835971a9c
나민수 <naminsu@naminsuui-MacBookPro.local>
2024-07-25 11:06:09 +0900

    level1_1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,9 +1,21 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Test 나민수 나민수");
+        Scanner scanner1 = new Scanner(System.in);
+        //1번쨰 숫자 입력 받기
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = scanner1.nextInt();
+        System.out.println("첫번째 숫자 : " + num1);
+
+        //2번쨰 숫자 입력 받기
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = scanner1.nextInt();
+        System.out.println("두번째 숫자 : " + num2);
+
     }
 }
 //
