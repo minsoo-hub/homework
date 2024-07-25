17a15b2e5a2ad7946aaeeffcaf5c736d8b3d448b
나민수 <naminsu@naminsuui-MacBookPro.local>
2024-07-25 14:13:19 +0900

    Level1_3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -17,12 +17,36 @@ public class App {
         int num2 = sc.nextInt();
         System.out.println("두번째 숫자 : " + num2);
 
-
         //Level1_2
         System.out.print("사칙연산 기호를 입력해주세요: ");
         char op = sc.next().charAt(0);
         System.out.println("입력한 사칙연산 기호 :" + op);
 
+        //Level1_3
+        int result =0;
+        switch (op){
+            case '+' :
+                result = num1 + num2;
+                break;
+            case '-' :
+                result = num1 - num2;
+                break;
+            case '*' :
+                result = num1 * num2;
+                break;
+            case '/' :
+                if (num2 == 0){
+                    System.out.println("나눗셈 연산에서 분모에 0이 입력이 불가능입니다.");
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                System.out.println("다시 입력해주세요");
+                break;
+        }
+
+        System.out.println("결과 :" + result);
 
     }
 }
