f44fe7d6ffaf278a1566a379969f91d6794b351a
나민수 <naminsu@naminsuui-MacBookPro.local>
2024-07-29 20:39:14 +0900

    Level2_1 예외처리 잘한지 모르겠어서 뒤에서 더 확인하겠습니다

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,35 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
+    public Calculator() {
+        ArrayList<Double> results = new ArrayList<>();
+    }
+
+    public double calculate(int num1, int num2, char op) throws DivisionException {
+        double result;
+        switch (op){
+            case '+' :
+                result = num1 + num2;
+                break;
+            case '-' :
+                result = num1 - num2;
+                break;
+            case '*' :
+                result = num1 * num2;
+                break;
+            case '/' :
+                if (num2 == 0){
+                    throw new DivisionException("나눗셈 연산에서 분모에 0이 입력이 불가능입니다.");
+                } else {
+                    result = (double)num1 / num2;
+                }
+                break;
+            default:
+                throw new DivisionException("다시 입력해주세요");
+        }
+        return result;
+    }
 }
---
+++ src/main/java/calculator/DivisionException.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class DivisionException extends Exception{
+    public DivisionException(String message) {
+        super(message);
+    }
+}
